bbe23d726b29045b73d345b2d5fcddc6d05524e5
jwpark <haze@haze.kr>
2021-09-17 14:26:47 +0900

    initial

--- src/com/jwparks/movieplugin/MovieStart.java
+++ src/com/jwparks/movieplugin/MovieStart.java
@@ -15,16 +15,15 @@ public class MovieStart implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         String url = "plugins/Movies/"+args[0]+".html";
-        System.out.println("String check: True: "+ url);
         File htmlFile = new File(url);
-        System.out.println("File load check: True");
         try {
             Desktop.getDesktop().browse(htmlFile.toURI());
-            System.out.println("Open browse check: True");
         } catch (IOException e) {
             e.printStackTrace();
         }
         return true;
     }
 }
+
+
 
